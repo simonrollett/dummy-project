d9e9d7e2e98d3dd44de9252db9ff5bdbb2b53989
simonrollett <srollett@qmetric.co.uk>
2013-11-06 13:00:09 +0000

    Added new assets

---
+++ index-not-used.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Dummy Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+   <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+
+  </head>
+
+  <body>
+
+      <nav>
+          <div class="nav span12">
+              <h1>Hello</h1>
+          </div>
+      </nav>
+
+
+
+    <!-- Footer  -->
+
+    <footer class="footer">
+        <div class="container">
+        footer
+        </div>
+    </footer>
+
+
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+    <script src="assets/js/bootstrap-affix.js"></script>
+
+
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Dummy Project</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!--    <link href="assets/css/bootstrap.css" rel="stylesheet">-->
+<!--    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">-->
+<link href="http://helpmeit.co.uk/client/hydreco/css/base.css" rel="stylesheet">
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="assets/js/html5shiv.js"></script>
+<![endif]-->
+<link rel="shortcut icon" href="assets/ico/favicon.png">
+
+
+</head>
+
+<body>
+
+    <div class="site-wrapper site-width">
+
+        <header class="header-wrapper">
+            <div class="header">
+                <div class="float-anchor">
+                    <h1>logo bar</h1>
+                </div>
+            </div>
+        </header>
+
+        <nav class="nav-wrapper">
+            <div class="nav">
+                <div class="float-anchor">
+                nav
+                </div>
+            </div>
+        </nav>
+
+
+
+        <!-- Footer  -->
+
+        <footer class="footer-wrapper">
+            <div class="footer">
+                <div class="float-anchor">
+                    footer
+                </div>
+            </div>
+        </footer>
+    </div>
+
+
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+  </body>
+</html>
