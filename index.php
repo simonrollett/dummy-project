919e6170e52d83773e7d3226fccdc5d8d035b94d
simonrollett <srollett@qmetric.co.uk>
2013-11-14 09:13:21 +0000

    Updated accordion fun styles

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
                 <div class="navigation-accordion-wrapper">
                     <ul class="nav-row navigation navigation-accordion">
                         <li class="nav-link-wrapper align-center nav-title"><a class="nav-link" href="#">DATE:</a></li>
-                        <li class="nav-link-wrapper"><a href="#" class="nav-link accordion-options-achievements">Achievements</a></li>
+                        <li class="nav-link-wrapper nav-selected"><a href="#" class="nav-link accordion-options-achievements">Achievements</a></li>
                         <li class="nav-link-wrapper"><a href="#" class="nav-link accordion-options-work">Work</a></li>
                         <li class="nav-link-wrapper"><a href="#" class="nav-link accordion-options accordion-options-fun">Fun</a></li>
                     </ul>
                 </div>
 
-                <ul class="accordion accordion-timeline accordion-items-work">
+                <ul class="accordion accordion-timeline accordion-items-achievements">
 
                     <?php include "includes/accordion-work-qmetric.php";?>
 
