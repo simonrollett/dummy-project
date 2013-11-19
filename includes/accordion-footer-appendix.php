5232b04c928b769c9f10abeb25e0c2b68bb6cefc
rollett <srollett@qmetric.co.uk>
2013-11-19 18:36:59 +0000

    Updated arrow colour and other styles

--- includes/accordion-footer-appendix.php
+++ includes/accordion-footer-appendix.php
@@ -1,3 +1,3 @@
 <div class="wrapper-contact align-right">
-    <a class="btn btn-contact" href="contact.php">Contact</a>
+    <a class="btn btn-contact" href="contact.php">Contact<span class="icon icon-nav"></span></a>
 </div>
